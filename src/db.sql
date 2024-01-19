a3d5e8b3d22006f4b3ed3eab43eb25e105fdd117
cg10036 <cg10036@gmail.com>
2024-01-20 00:17:05 +0900

    fix

--- src/db.sql
+++ src/db.sql
@@ -1,4 +1,6 @@
 CREATE USER 'test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'test1234!@';
+CREATE USER 'test'@'%' IDENTIFIED WITH mysql_native_password BY 'test1234!@';
 CREATE DATABASE `test`;
 GRANT ALL PRIVILEGES ON `test`.* TO 'test'@'localhost';
+GRANT ALL PRIVILEGES ON `test`.* TO 'test'@'%';
 FLUSH PRIVILEGES;
