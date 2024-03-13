90175c9bbb8e1d0240196d66ede13a29a3ba2cd8
cg10036 <cg10036@gmail.com>
2024-03-13 23:23:06 +0900

    fix

--- src/services/material.service.js
+++ src/services/material.service.js
@@ -86,11 +86,11 @@ const pdf = async (res, id) => {
     },
     select: ["file"],
   });
-  if (!file) {
+  if (!file || !id) {
     return res.status(404).send("File not found");
   }
   res.setHeader("Content-Type", "application/pdf");
-  return res.send(base64.decode(file));
+  return res.send(Buffer.from(file, "base64"));
 };
 
 module.exports = {
