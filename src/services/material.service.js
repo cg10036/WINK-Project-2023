1c0597aa32cb44cba6a28c64da93d89fe1dcd615
Ready-Bridge <hanjungyo5533@gmail.com>
2024-03-13 22:03:05 +0900

    Add : material

--- src/services/material.service.js
+++ src/services/material.service.js
@@ -2,8 +2,17 @@ const { HttpResponse } = require("../helpers/response.helper");
 const Material = require("../models/material.model");
 const MaterialRepository = require("../repositories/material.repository");
 
-const send = async (sql, prepare) => {
-  const [result] = await materialRepository.execute(sql, prepare);
+const send = async (userId, title, content, file, professor) => {
+  let material = new Material();
+  material.userId = userId;
+  material.title = title;
+  material.file = file;
+  material.content = content;
+  material.professor = professor;
+
+  MaterialRepository.save(material);
+
+  return new HttpResponse(201, "UPLOAD_SUCCESS");
 };
 
 module.exports = {
