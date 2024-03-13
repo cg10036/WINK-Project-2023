705d1baf0e4ccfcb06861892925748d57e81d1c3
cg10036 <cg10036@gmail.com>
2024-03-13 23:32:02 +0900

    fix

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -3,10 +3,10 @@ const authHelper = require("../helpers/auth.helper");
 const router = express.Router();
 
 router.use("/auth", require("./auth.route"));
+router.use("/material", require("./material.route.js"));
 router.use(authHelper.middleware);
 router.use("/me", require("./me.route"));
 router.use("/ping", require("./ping.route"));
 router.use("/studymate", require("./studyMate.route"));
-router.use("/material", require("./material.route.js"));
 
 module.exports = router;
--- src/routes/material.route.js
+++ src/routes/material.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const materialController = require("../controllers/material.controller");
 
+router.get("/pdf", materialController.pdf);
+router.use(authHelper.middleware);
 router.post("/send", materialController.send);
 router.get("/posts", materialController.posts);
 router.get("/detail", materialController.detail);
-router.get("/pdf", materialController.pdf);
 
 module.exports = router;
